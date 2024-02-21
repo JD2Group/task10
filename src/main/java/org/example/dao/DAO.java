1905a35e118dea8b650856c0ff53326adf7f7340
Timon <Timon321.86@gmail.com>
2024-02-21 17:55:44 +0300

    Add service methods for all review.

--- src/main/java/org/example/dao/DAO.java
+++ src/main/java/org/example/dao/DAO.java
@@ -1,16 +1,17 @@
 package org.example.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public interface DAO<T> {
+public interface DAO<T,R> {
 
-    void create(T t);
+    T create(T t);
 
-    T read(Object id);
+    T read(R id);
 
     void update(T t);
 
-    void delete(Object id);
+    void delete(R id);
 
     List<T> readAll();
 }
