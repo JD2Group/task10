7fd8d24bf0f3a4aa4b9c448568fadd05382ea480
Olga Tysevich <Olga.Tisevich@gmail.com>
2024-02-25 11:49:19 +0300

    updated AdministratorServiceImplTest

--- src/test/java/org/example/utils/MockConstants.java
+++ src/test/java/org/example/utils/MockConstants.java
@@ -5,18 +5,18 @@ import java.util.Random;
 public abstract class MockConstants {
     public static final int MAX_RANDOM_NUMBER = 100;
     public static final Random RANDOM = new Random();
-    public static final String COURSE_TITLE = "Course title No.";
-    public static final String PROFESSOR_NAME = "Professor name No.";
-    public static final String PROFESSOR_SURNAME = "Professor surname  No.";
-    public static final String PROFESSOR_EMAIL = "Professor No.%d email  No.";
-    public static final String RESPONSE = "response No.";
-    public static final String REVIEW = "review No.";
+    public static final String COURSE_TITLE_PATTERN = "Course title No.";
+    public static final String PROFESSOR_NAME_PATTERN = "Professor name No.";
+    public static final String PROFESSOR_SURNAME_PATTERN = "Professor surname  No.";
+    public static final String PROFESSOR_EMAIL_PATTERN = "Professor No.%d email  No.";
+    public static final String RESPONSE_PATTERN = "response No.";
+    public static final String REVIEW_PATTERN = "review No.";
     public static final int MARK = 5;
-    public static final String STUDENT_NAME = "Student No.";
-    public static final String STUDENT_SURNAME = "Student surname No.";
-    public static final String STUDENT_EMAIL = "Student No.%d email  No.";
-    public static final String TASK_TITLE = "Task title No.";
-    public static final String TASK_DESCRIPTION = "Task description No.";
+    public static final String STUDENT_NAME_PATTERN = "Student No.";
+    public static final String STUDENT_SURNAME_PATTERN = "Student surname No.";
+    public static final String STUDENT_EMAIL_PATTERN = "Student No.%d email  No.";
+    public static final String TASK_TITLE_PATTERN = "Task title No.";
+    public static final String TASK_DESCRIPTION_PATTERN = "Task description No.";
     public static final int MAX_LIST_SIZE = 5;
     public static final String UPDATE = "updated";
     public static final String DELETE_ALL_COURSES = "delete from courses";
@@ -29,5 +29,5 @@ public abstract class MockConstants {
     public static final String GET_NUMBER_OF_PROFESSORS_QUERY = "select count(*) from professors";
     public static final String GET_NUMBER_OF_COURSES_QUERY = "select count(*) from courses";
     public static final String GET_NUMBER_OF_COURSES_BY_PROFESSOR_QUERY = "select count(*) from courses where professor_id = ?1";
-    public static final Long PROFESSOR_ID = 1L;
+    public static final String PROFESSOR_EMAIL = "test email";
 }
