6ea3588b03dee2304ea762e265f47ba4a4ac8118
Olga Tysevich <Olga.Tisevich@gmail.com>
2024-02-26 14:10:21 +0300

    updated all tests

--- src/test/java/org/example/servise/impl/ProfessorServiceImplTest.java
+++ src/test/java/org/example/servise/impl/ProfessorServiceImplTest.java
@@ -3,11 +3,14 @@ package org.example.servise.impl;
 import org.example.dao.*;
 import org.example.dao.impl.*;
 import org.example.pojo.*;
+import org.example.servise.AdminService;
 import org.example.servise.ProfessorService;
 import org.example.utils.MockUtils;
 import org.example.utils.QueryManager;
 import org.junit.jupiter.api.*;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.example.utils.MockConstants.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,11 +18,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProfessorServiceImplTest {
     private QueryManager queryManager = new QueryManager();
     private ProfessorService service = new ProfessorServiceImpl();
-    private Professor professor;
-    private Course course;
-    private Student student;
-    private Task task;
-    private Solution solution;
+    private AdminService adminService = new AdminServiceImpl();
+    private SolutionDao solutionDao;
+    private StudentDao studentDao;
+    private CourseDao courseDao;
+    private TaskDao taskDao;
 
     @BeforeAll
     public static void createData() {
@@ -37,21 +40,24 @@ class ProfessorServiceImplTest {
 
     @BeforeEach
     public void getData() {
-        List<Solution> solutions = queryManager.getList(GET_SOLUTIONS_QUERY, Solution.class);
-        solution = solutions.get(RANDOM.nextInt(solutions.size()));
-        task = solution.getTask();
-        student = solution.getStudent();
-        course = task.getCourse();
-        professor = course.getProfessor();
+        studentDao = new StudentDaoImpl();
+        solutionDao = new SolutionDaoImpl();
+        courseDao = new CourseDaoImpl();
+        taskDao = new TaskDaoImpl();
     }
 
     @AfterEach
     public void closeQueryManager() {
+        studentDao.closeManager();
+        solutionDao.closeManager();
+        courseDao.closeManager();
+        taskDao.closeManager();
         queryManager.closeSession();
     }
 
     @Test
     void getMyCourses() {
+        Professor professor = adminService.getProfessorByEmail(PROFESSOR_EMAIL);
         int expected = queryManager.getNumberOfObjects(GET_NUMBER_OF_COURSES_BY_PROFESSOR_QUERY, professor.getId());
         List<Course> actual = service.getMyCourses(professor);
 
@@ -60,6 +66,8 @@ class ProfessorServiceImplTest {
 
     @Test
     void getAllTasks() {
+        List<Course> courses = adminService.getAllCourses();
+        Course course = courses.get(RANDOM.nextInt(courses.size()));
         int expected = queryManager.getNumberOfObjects(GET_NUMBER_OF_TASKS_BY_COURSE, course.getId());
         List<Task> actual = service.getAllTasks(course);
 
@@ -68,6 +76,8 @@ class ProfessorServiceImplTest {
 
     @Test
     void getAllStudents() {
+        List<Course> courses = adminService.getAllCourses();
+        Course course = courses.get(RANDOM.nextInt(courses.size()));
         int expected = queryManager.getNumberOfObjects(GET_NUMBER_OF_STUDENTS_BY_COURSE_QUERY, course.getId());
         List<Student> actual = service.getAllStudents(course);
 
@@ -76,32 +86,42 @@ class ProfessorServiceImplTest {
 
     @Test
     void getSolution() {
-        Solution actual = service.getSolution(student, task);
+        List<Solution> solutions = solutionDao.readAll();
+        Solution expected = solutions.get(RANDOM.nextInt(solutions.size()));
+        Solution actual = service.getSolution(expected.getStudent(), expected.getTask());
 
-        assertTrue(student.getSolutions().contains(actual));
+        assertEquals(expected, actual);
     }
 
     @Test
     void getAllReadySolutions() {
-        int expected = (int) task.getSolutions().stream()
-                .filter(Solution::getReadyForReview).count();
-        int actual = service.getAllReadySolutions(task).size();
+        List<Solution> solutions = solutionDao.readAll().stream()
+                .filter(Solution::getReadyForReview)
+                .collect(Collectors.toList());
+        Solution solution = solutions.get(RANDOM.nextInt(solutions.size()));
+        Task task = solution.getTask();
+        List<Solution> readySolutions = solutionDao.getAllReadyByTask(task);
+        int expected = readySolutions.size();
+        int actual = service.getAllReadySolutions(solution.getTask()).size();
 
         assertEquals(expected, actual);
     }
 
     @Test
     void addTasks() {
+        List<Course> courses = courseDao.readAll();
+        Course course = courses.get(RANDOM.nextInt(courses.size()));
         Task expected = MockUtils.generateTask();
-        service.addTasks(course, expected);
+//        expected = service.addTasks(course, expected);
         List<Task> actual = service.getAllTasks(course);
 
-        assertTrue(actual.contains(task));
+        assertTrue(actual.contains(expected));
     }
 
     @Test
     void updateTask() {
-        TaskDao taskDao = new TaskDaoImpl();
+        List<Task> tasks = taskDao.readAll();
+        Task task = tasks.get(RANDOM.nextInt(tasks.size()));
         service.updateTask(task, UPDATE, UPDATE);
         Task actual = taskDao.read(task.getId());
 
@@ -111,16 +131,18 @@ class ProfessorServiceImplTest {
 
     @Test
     void deleteTask() {
-        TaskDao taskDao = new TaskDaoImpl();
+        List<Task> tasks = taskDao.readAll();
+        Task task = tasks.get(RANDOM.nextInt(tasks.size()));
         service.deleteTask(task);
-        Task actual = taskDao.read(task.getId());
+        List<Task> actual = service.getAllTasks(task.getCourse());
 
-        assertNull(actual);
+        assertFalse(actual.contains(task));
     }
 
     @Test
     void review() {
-        SolutionDao solutionDao = new SolutionDaoImpl();
+        List<Solution> solutions = solutionDao.readAll();
+        Solution solution = solutions.get(RANDOM.nextInt(solutions.size()));
         service.review(solution, MARK, REVIEW);
         Solution actual = solutionDao.read(solution.getId());
 
